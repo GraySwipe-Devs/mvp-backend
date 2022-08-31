22064f4e08f5bd6c73fb6e42e8a731db358ee5d1
Ishan <ishanjain1502@gmail.com>
2022-08-31 23:18:33 +0530

    models added

---
+++ models/salon.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const salonSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    ownerName: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    phoneNumber : {
+        type : String,
+        unique: true,
+        required: true
+    },
+    services :{
+        type: Map,
+        of: String
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  });
+  
+  const Salon = mongoose.model("Salon", salonSchema);
+  module.exports = Salon;
