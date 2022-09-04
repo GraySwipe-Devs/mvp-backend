9a6aa8363e3113db3735f8f05f3dcf67749fd22e
Ishan <ishanjain1502@gmail.com>
2022-09-04 22:21:02 +0530

    get salons working

--- routes/v1/api.js
+++ routes/v1/api.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const signup = require('./salon/signup.salon');
 const signin = require('./salon/signin.salon');
+const getSalon = require('./salon/getSalon.salon');
 
 router.use("/v1/salon/register" , signup);
 router.use("/v1/salon/login" , signin);
+router.use("/v1/salons/getsalon" , getSalon);
 
 module.exports = router;
